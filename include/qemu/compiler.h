ad0b9a78d486eb0010587731f88c0b576cbb7b4a
Truls Asheim <truls@asheim.dk>
2019-11-12 14:00:01 +0100

    Resolve bound equals destination size warnings.
    Mirrors upstream qemu

--- include/qemu/compiler.h
+++ include/qemu/compiler.h
@@ -42,6 +42,21 @@
 
 #define QEMU_ALIGNED(X) __attribute__((aligned(X)))
 
+/*
+ * The nonstring variable attribute specifies that an object or member
+ * declaration with type array of char or pointer to char is intended
+ * to store character arrays that do not necessarily contain a terminating
+ * NUL character. This is useful in detecting uses of such arrays or pointers
+ * with functions that expect NUL-terminated strings, and to avoid warnings
+ * when such an array or pointer is used as an argument to a bounded string
+ * manipulation function such as strncpy.
+ */
+#if __has_attribute(nonstring)
+# define QEMU_NONSTRING __attribute__((nonstring))
+#else
+# define QEMU_NONSTRING
+#endif
+
 #ifndef glue
 #define xglue(x, y) x ## y
 #define glue(x, y) xglue(x, y)
